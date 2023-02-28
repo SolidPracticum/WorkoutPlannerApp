18e5746527e313ce0df393e32c71935a4b2ed091
lakuuu <hhh079494@gmail.com>
2023-02-28 20:36:38 +0800

    feat: add header register

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,9 @@
 import { useTranslation } from "react-i18next";
+import scss from "./Header.module.scss";
 import "./../../i18n";
+import image from "./Vector.png";
+import img from "./Vector1.png";
+import elp from "./Ellipse.png";
 
 export default function Header() {
   const { t, i18n } = useTranslation();
@@ -8,9 +12,18 @@ export default function Header() {
   };
   return (
     <div>
-      <h1>{t("header.title")}</h1>
-      <button onClick={() => changeLanguage("en")}>EN</button>
-      <button onClick={() => changeLanguage("ru")}>RU</button>
+      <div className={scss.reg}>
+        <button className={scss.account}>W</button>
+        <img className={scss.img} src={img} alt="notification" />
+        <img className={scss.image} src={image} alt="qestionMark" />
+        <img className={scss.ellipse} src={elp} alt="pfp" />
+        <p className={scss.name}>Aлинa</p>
+      </div>
+      <div>
+        <h1>{t("header.title")}</h1>
+        <button onClick={() => changeLanguage("en")}>EN</button>
+        <button onClick={() => changeLanguage("ru")}>RU</button>
+      </div>
     </div>
   );
 }
