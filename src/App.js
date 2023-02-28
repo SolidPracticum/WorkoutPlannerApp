5ad7c49dc20b1e513eeb87eaaef3392f5957d159
Aselshine <aasel9988@gmail.com>
2023-02-28 12:19:31 +0600

    feat:secondModal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-
+import SecondModal from "./components/SecondModal/SecondModal";
 function App() {
   return (
-    <>
+    <div>
       <Routes>
         <Route />
       </Routes>
-    </>
+      <SecondModal />
+    </div>
   );
 }
-
 export default App;
---
+++ src/components/SecondModal/SecondModal.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import styles from "./SecondModal.module.scss";
+const SecondModal = () => {
+  const [woman] = useState(true);
+  const [man] = useState(true);
+  const handleChange = () => {};
+  return (
+    <div className={styles.secondModal}>
+      <img className={styles.arrow} src="/Images/left-arrow.png" alt="" />
+      <img className={styles.close} src="/Images/close.png" alt="" />
+      <h1>Ваш пол?</h1>
+      <p>Отвечая на наши вопросы Вы поможете нам составить Ваш персональный план тренировок</p>
+      <div className={styles.womanIcon}>
+        <input
+          className={styles.first}
+          type="checkbox"
+          id={styles.checkbox1}
+          value={woman}
+          onChange={() => handleChange("woman")}
+        />{" "}
+        женский <br />
+        <label for="checkbox1">
+          <img src="/Images/woman.png" alt="" />
+        </label>
+      </div>
+      <div className={styles.manIcon}>
+        <input
+          type="checkbox"
+          id={styles.checkbox2}
+          value={man}
+          onChange={() => handleChange("man")}
+        />{" "}
+        мужской <br />
+        <label for="checkbox2">
+          <img src="/Images/man.png" alt="" />
+        </label>
+      </div>
+      <button>Далее</button>
+    </div>
+  );
+};
+export default SecondModal;
