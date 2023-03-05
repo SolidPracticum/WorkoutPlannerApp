38b21a3863d670130e7a12b96e1724bb0f71fbf7
lakuuu <hhh079494@gmail.com>
2023-03-06 01:39:13 +0800

    feat: headerRed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,34 @@
 import { useTranslation } from "react-i18next";
 import scss from "./Header.module.scss";
 import "./../../i18n";
-import image from "./Vector.png";
-import img from "./Vector1.png";
-import elp from "./Ellipse.png";
-
+import { useState } from "react";
+import question from "..//..//headerImg//Vector.png";
+import language from "..//..//headerImg//language.png";
 export default function Header() {
+  const [isLanguage, setLanguage] = useState(false);
   const { t, i18n } = useTranslation();
   const changeLanguage = (language) => {
     i18n.changeLanguage(language);
+  };
+  const [open] = useState(false);
+  const isChangeLanguage = () => {
+    setLanguage(!isLanguage);
+    if (isLanguage == true) {
+      changeLanguage("ru");
+    } else {
+      changeLanguage("en");
+    }
   };
   return (
     <div>
       <div className={scss.reg}>
         <button className={scss.account}>W</button>
-        <img className={scss.img} src={img} alt="notification" />
-        <img className={scss.image} src={image} alt="qestionMark" />
-        <img className={scss.ellipse} src={elp} alt="pfp" />
-        <p className={scss.name}>Aлинa</p>
-      </div>
-      <div>
-        <h1>{t("header.title")}</h1>
-        <button onClick={() => changeLanguage("en")}>EN</button>
-        <button onClick={() => changeLanguage("ru")}>RU</button>
+        <div className={scss.imgs}>
+          <img src={question} className={scss.notifs} alt="question" />
+          <img src={language} className={scss.notifs} alt="language" onClick={isChangeLanguage} />
+          <button className={scss.btn}>{t("reg")}</button>
+          <div className={!open ? scss.SelectTranslation : scss.acive}></div>
+        </div>
       </div>
     </div>
   );
---
+++ src/constants/header/constant.js
@@ -0,0 +1,10 @@
+export const data = [
+  {
+    id: 1,
+    img: "/images/header/Vector1.png"
+  },
+  {
+    id: 2,
+    img: "/images/header/language.png"
+  }
+];
