e0c1355316d342215cc8518c623bc3e4d7c21c57
Aselshine <aasel9988@gmail.com>
2023-03-06 16:20:51 +0600

    fix: attributes names the same

--- src/components/SecondModal/SecondModal.jsx
+++ src/components/SecondModal/SecondModal.jsx
@@ -8,8 +8,8 @@ export default function SecondModa() {
   const { t } = useTranslation();
   return (
     <div className={styles.secondModal}>
-      <img className={styles.arrow} src="/Images/left-arrow.png" alt="backArrow" />
-      <img className={styles.close} src="/Images/close.png" alt="close" />
+      <img className={styles.arrow} src="Images/SEcondModalIcons/left-arrow.png" alt="backArrow" />
+      <img className={styles.close} src="Images/SecondModalIcons/close.png" alt="close" />
       <h1>{t("reg.title")}</h1>
       <p>{t("reg.text")}</p>
 
@@ -23,7 +23,7 @@ export default function SecondModa() {
         <div className={styles.man}>
           {t("reg.checkbox2")} <br />
           <label htmlFor="checkbox2">
-            <img src="/Images/man1.png" alt="maleGender" />
+            <img src="Images/SecondModalIcons/man1.png" alt="maleGender" />
           </label>
         </div>
       </div>
@@ -38,7 +38,7 @@ export default function SecondModa() {
         <div className={styles.woman}>
           {t("reg.checkbox1")} <br />
           <label htmlFor="checkbox1">
-            <img src="/Images/woman.png" alt="womanGender" />
+            <img src="Images/SecondModalIcons/woman.png" alt="womanGender" />
           </label>
         </div>
       </div>
