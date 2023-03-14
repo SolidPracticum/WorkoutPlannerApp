ab789ad728c3bb09e2c1c8370a8cb449642aa156
Eldiyar Temirbekov <teldiyar98@gmail.com>
2023-03-14 18:57:23 +0600

    fix:fourthModal ,i18next,linter,quotes

--- src/components/FourthModal.jsx
+++ src/components/FourthModal.jsx
@@ -3,29 +3,14 @@ import InputModal from "./InputModal";
 import { toggle } from "../redux/Slice";
 import { useDispatch, useSelector } from "react-redux";
 import "./FourthModal.scss";
-import { useTranslation } from "react-i18next";
 
 function FourthModal() {
-  const { t, i18n } = useTranslation();
-  const changeLanguage = (language) => {
-    i18n.changeLanguage(language);
-  };
-
   const modal = useSelector((state) => state.slice.modal);
   const dispatch = useDispatch();
   const toggleCounter = () => {
     dispatch(toggle());
   };
-  return (
-    <div className="modal">
-      <div>
-        <button onClick={() => changeLanguage("en")}>en</button>
-        <button onClick={() => changeLanguage("ru")}>ru</button>
-        <div>{t("text")}</div>
-      </div>
-      {modal && <InputModal toggleCounter={toggleCounter} />}
-    </div>
-  );
+  return <div className="modal">{modal && <InputModal toggleCounter={toggleCounter} />}</div>;
 }
 
 export default FourthModal;
