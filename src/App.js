240077a4388978ed5e3d7c810daf1639f8e780a1
Eldiyar Temirbekov <teldiyar98@gmail.com>
2023-03-14 15:25:15 +0600

    feat:fourthModal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
-import React from "react";
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import './App.css';
-import './App.scss';
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-      </Routes>
-    </div>
-  );
-}
 
-export default App;
+import FourthModal from './components/FourthModal'
+import './App.scss'
+function App() {
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/fourth' element={<FourthModal />} />
+			</Routes>
+		</div>
+	)
+}
+export default App
